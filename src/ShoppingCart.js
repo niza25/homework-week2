286ce37e09cc4eba0a0471b3d6dad656e40b4a5e
Izabela <izabela.nowosada@gmail.com>
2019-01-18 22:58:08 +0100

    add small changes

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,31 +1,35 @@
 class ShoppingCart {
     constructor() {
-        this.items = []
+        this.items = [];
     }
+
     getItems() {
-        return this.items
+        return this.items;
     }
+
     addItem(itemName, quantity, price) {
-        const item = {};
-        item.name = itemName;
-        item.quantity = quantity;
-        item.pricePerUnit = price;
+        const item = {
+            name: itemName,
+            quantity: quantity,
+            pricePerUnit: price
+        };
+
         this.items.push(item);
     }
+
     total() {
         if (this.items.length > 0) {
             return this.items.reduce((acc, curr) => {
-                let sum = acc + (curr.quantity * curr.pricePerUnit)
-                return sum
+                let sum = acc + (curr.quantity * curr.pricePerUnit);
+                return sum;
             }, 0)
         } else {
-            return 0
+            return 0;
         }
-
     }
+
     clear() {
-        this.items = []
-        return this.items
+        this.items = [];
     }
 }
 
